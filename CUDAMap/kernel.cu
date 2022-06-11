4c16c87f0f71a8284f84cd88a73d2e950bd30284
Matthew M <blazinggunner@hotmail.com>
2022-06-11 03:36:38 -0500

    Update kernel.cu
    Added proper global kernel support, significantly trimmed down abstraction bloat, reduced complexity and ported to raw pointers for the values and hash tables as well.  Added overrides standard for unified memory contexts that allow for the new keyword, as well as the delete keyword. Refactored generally to make the map operate as a pointer and to be dealt with via CudaDeviceReset properly.  Acts as a minimal overhead fixed-size hashmap for the purposes of accessing and modifying data without having to frequently linearize and repack data between global kernel calls and ends.

--- CUDAMap/kernel.cu
+++ CUDAMap/kernel.cu
@@ -1,36 +1,30 @@
-﻿#include "device_launch_parameters.h"
+#include "device_launch_parameters.h"
 #include "hashmap.cuh"
 
 #include <iostream>
 
 #include <stdio.h>
 
-__global__ void TestKernel(float* result, HashMap<float, float, HashFunc<float>>& map)
+__global__ void TestKernel(HashMap<float, float, HashFunc<float>>* map)
 {
-    float output = 1;
-    result = &output;
+    printf("%f", map->Get(4.0f));
 }
 
 int main()
 {
-    HashMap<float, float, HashFunc<float>> map;
+    cudaError_t cuda_status = cudaSuccess;
 
-    cudaError_t cuda_status = cudaSetDevice(0);
+    HashMap<float, float, HashFunc<float>>* map = new HashMap<float, float, HashFunc<float>>();
 
-    if (cuda_status != cudaSuccess) {
-        fprintf(stderr, "CudaSetDevice failed!");
-        return 1;
-    }
+    function<cudaError_t()> set_device_func = []() { return cudaSetDevice(0); };
+    cuda_status = WrapperFunction(set_device_func, "Main", "GPUSetDevice", cuda_status, "");
 
-    map.Put(2.0f, 2.0f);
-    map.Put(3.0f, 100.0f);
-    map.Put(3.0f, 120.0f);
+    map->Put(2.0f, 2.0f);
+    map->Put(3.0f, 100.0f);
+    map->Put(4.0f, 120.0f);
+    map->Put(1.0f, 1220.0f);
 
-    float* result;
-    cudaMalloc(&result, sizeof(float));
-
-    TestKernel<<<1, 1>>> (result, map);
-
+    TestKernel<<<1, 1>>> (map);
 
     function<cudaError_t()> error_func = []() { return cudaGetLastError(); };
     cuda_status = WrapperFunction(error_func, "Main", "GPUAccessLastError", cuda_status, "");
@@ -38,12 +32,10 @@ int main()
     function<cudaError_t()> sync_func = []() { return cudaDeviceSynchronize(); };
     cuda_status = WrapperFunction(sync_func, "Main", "GPUAccessSyncFunc", cuda_status, "");
 
-    map.~HashMap();
+    delete map;
 
-    cuda_status = cudaDeviceReset();
-    if (cuda_status != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceReset failed!");
-        return 1;
-    }
+    function<cudaError_t()> reset_func = []() { return cudaDeviceReset(); };
+    cuda_status = WrapperFunction(reset_func, "Main", "GPUDeviceReset", cuda_status, "");
+
     return 0;
 }
